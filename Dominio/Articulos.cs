6fc729701bbe313feb7f58cfbda0c0d64dc4a788
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 13:36:31 -0300

    creacion de las clase  Articulo y Marca

--- Dominio/Articulos.cs
+++ Dominio/Articulos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace Dominio
 {
     public class Articulos
     {
+        private int id;
+        private string codigo;
+        private string nombre;
+        private string descripcion;
+        private string imagenUrl;
+        private decimal precio;
+        private Marca marca;
+        private Categoria categoria;
+
+        public int Id { get => id; set => id = value; }
+        [DisplayName("Código")]
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string ImagenUrl { get => imagenUrl; set => imagenUrl = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+        public Marca Marca { get => marca; set => marca = value; }
+        [DisplayName("Categoría")]
+        public Categoria Categoria { get => categoria; set => categoria = value; }
+
+
     }
 }
---
+++ Dominio/Marca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+   public class Marca
+    {
+        private int Id;
+        private string Descripcion;
+
+        [DisplayName("Id")]
+        public int Id1 { get => Id; set => Id = value; }
+        [DisplayName("Descripción")]
+        public string Descripcion1 { get => Descripcion; set => Descripcion = value; }
+
+        //Sobreescribimos metodo
+        public override string ToString()
+        {
+            return Descripcion1;
+        }
+    }
+}
