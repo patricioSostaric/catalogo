786dd7be1976895bfb43c49b27687fa42fdf28f8
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 14:02:36 -0300

    creacion de las clase CategoriaNegocio y MarcaNegocio

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> listaCategorias = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id, Descripcion from CATEGORIAS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria art = new Categoria();
+                    art.Id1 = (int)datos.Lector["Id"];
+                    art.Descripcion1 = (string)datos.Lector["Descripcion"];
+                    listaCategorias.Add(art);
+
+                }
+                return listaCategorias;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+        }
+        public bool chequear(List<Categoria> lista, string categoria)
+        {
+            bool existe = false;
+
+            foreach (var Categoria in lista)
+            {
+                if (Categoria.Descripcion1.ToUpper() == categoria.ToUpper())
+                {
+                    existe = true;
+                }
+            }
+            return existe;
+        }
+        public void agregarCat(Categoria categ)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Insert into CATEGORIAS(Descripcion) VALUES('" + categ.Descripcion1 + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+        }
+        public void eliminarCat(Categoria cat)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Delete from CATEGORIAS where Id = " + cat.Id1);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+        }
+        public bool validarCampos(string campo)
+        {
+            if (string.IsNullOrEmpty(campo))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool validarLetras(string campo)
+        {
+            foreach (char letra in campo)
+            {
+                if (char.IsNumber(letra))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Dominio;
+
+namespace Negocio
+{
+   public  class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> listaMarcas = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id, Descripcion from MARCAS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca art = new Marca();
+                    art.Id1 = (int)datos.Lector["Id"];
+                    art.Descripcion1 = (string)datos.Lector["Descripcion"];
+                    listaMarcas.Add(art);
+                }
+                return listaMarcas;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+
+        }
+        public bool chequear(List<Marca> lista, string marca)
+        {
+            bool existe = false;
+            foreach (var Marca in lista)
+            {
+                if (Marca.Descripcion1.ToUpper() == marca.ToUpper())
+                {
+                    existe = true;
+                }
+            }
+            return existe;
+        }
+        public void agregarMarca(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+
+            try
+            {
+                datos.setearConsulta("insert into MARCAS(Descripcion) values('" + marca.Descripcion1 + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+
+
+        }
+        public void eliminarMarca(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Delete from Marcas where Id = " + marca.Id1);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrar();
+            }
+        }
+        public bool validarCampos(string campo)
+        {
+            if (string.IsNullOrEmpty(campo))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool validarLetras(string campo)
+        {
+            foreach (char letra in campo)
+            {
+                if (char.IsNumber(letra))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
