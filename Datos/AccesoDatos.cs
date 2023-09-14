9ede60d7990bfefadda750b2b705a46d7a246334
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 13:28:19 -0300

    creacion de las clase AccesoDatos

--- Datos/AccesoDatos.cs
+++ Datos/AccesoDatos.cs
@@ -10,5 +10,69 @@ namespace Datos
 {
     public class AccesoDatos
     {
+        private SqlCommand comando = new SqlCommand();
+        private SqlDataReader lector;
+        public SqlDataReader Lector { get { return lector; } }
+        private SqlConnection conexion = new SqlConnection("server =.\\SQLEXPRESS; database = CATALOGO_DB; integrated security = true");
+        public SqlConnection abrir()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public SqlConnection cerrar()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+            return conexion;
+        }
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setearParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
     }
 }
