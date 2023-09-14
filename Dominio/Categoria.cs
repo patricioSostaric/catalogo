35319f3577f72dc4ee4432023d378588e7a12c2c
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 13:44:16 -0300

    creacion de las clase  Categoria

---
+++ Dominio/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+     public class Categoria
+    {
+        private int Id;
+        private string Descripcion;
+
+        [DisplayName("Id")]
+        public int Id1 { get => Id; set => Id = value; }
+        [DisplayName("Descripción")]
+        public string Descripcion1 { get => Descripcion; set => Descripcion = value; }
+
+
+        public override string ToString()
+        {
+            return Descripcion1;
+        }
+    }
+}
