55df0be395624be866dd3905d3b5a85af714cc8d
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 16:59:21 -0300

    se le agrego la funcionalidad al boton  agregar categoria , app 100 % funcional

--- formularios/FrmNuevaMarca.Designer.cs
+++ formularios/FrmNuevaMarca.Designer.cs
@@ -29,12 +29,115 @@ namespace formularios
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregarNew = new System.Windows.Forms.Button();
+            this.txtMarcAgr = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.dgvMarcas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMarcas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(397, 102);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(71, 25);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Borrar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnAgregarNew
+            // 
+            this.btnAgregarNew.Location = new System.Drawing.Point(309, 102);
+            this.btnAgregarNew.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregarNew.Name = "btnAgregarNew";
+            this.btnAgregarNew.Size = new System.Drawing.Size(71, 25);
+            this.btnAgregarNew.TabIndex = 16;
+            this.btnAgregarNew.Text = "Agregar";
+            this.btnAgregarNew.UseVisualStyleBackColor = true;
+            this.btnAgregarNew.Click += new System.EventHandler(this.btnAgregarNew_Click);
+            // 
+            // txtMarcAgr
+            // 
+            this.txtMarcAgr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMarcAgr.Location = new System.Drawing.Point(309, 65);
+            this.txtMarcAgr.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMarcAgr.MaxLength = 50;
+            this.txtMarcAgr.Name = "txtMarcAgr";
+            this.txtMarcAgr.Size = new System.Drawing.Size(159, 21);
+            this.txtMarcAgr.TabIndex = 14;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(293, 37);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(91, 15);
+            this.lbl2.TabIndex = 13;
+            this.lbl2.Text = "Agregar marca:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(30, 15);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(51, 15);
+            this.lbl1.TabIndex = 12;
+            this.lbl1.Text = "Marcas:";
+            // 
+            // dgvMarcas
+            // 
+            this.dgvMarcas.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvMarcas.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvMarcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMarcas.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvMarcas.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgvMarcas.Location = new System.Drawing.Point(11, 37);
+            this.dgvMarcas.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvMarcas.MultiSelect = false;
+            this.dgvMarcas.Name = "dgvMarcas";
+            this.dgvMarcas.RowHeadersWidth = 62;
+            this.dgvMarcas.RowTemplate.Height = 28;
+            this.dgvMarcas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMarcas.Size = new System.Drawing.Size(278, 233);
+            this.dgvMarcas.TabIndex = 11;
+            // 
+            // FrmNuevaMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmNuevaMarca";
+            this.ClientSize = new System.Drawing.Size(551, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregarNew);
+            this.Controls.Add(this.txtMarcAgr);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.dgvMarcas);
+            this.MaximumSize = new System.Drawing.Size(567, 489);
+            this.MinimumSize = new System.Drawing.Size(567, 489);
+            this.Name = "FrmNuevaMarca";
+            this.Text = "Nueva Marca";
+            this.Load += new System.EventHandler(this.FrmNuevaMarca_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMarcas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregarNew;
+        private System.Windows.Forms.TextBox txtMarcAgr;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.DataGridView dgvMarcas;
     }
 }
--- formularios/FrmNuevaMarca.cs
+++ formularios/FrmNuevaMarca.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
+using Negocio;
 
 namespace formularios
 {
     public partial class FrmNuevaMarca : Form
     {
+        List<Marca> listaMarcas = new List<Marca>();
         public FrmNuevaMarca()
         {
             InitializeComponent();
         }
+
+        private void FrmNuevaMarca_Load(object sender, EventArgs e)
+        {
+            cargarMarcas();
+        }
+        private void cargarMarcas()
+        {
+            try
+            {
+                MarcaNegocio marcas = new MarcaNegocio();
+
+                listaMarcas = marcas.listar();
+                dgvMarcas.DataSource = listaMarcas;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+        }
+
+        private void btnAgregarNew_Click(object sender, EventArgs e)
+        {
+            Marca marca = new Marca();
+            MarcaNegocio negocio = new MarcaNegocio();
+
+            try
+            {
+                if (negocio.validarCampos(txtMarcAgr.Text))
+                {
+                    MessageBox.Show("Debe ingresar una marca");
+                    return;
+                }
+                if (negocio.validarLetras(txtMarcAgr.Text))
+                {
+                    MessageBox.Show("Ingrese letras, por favor");
+                    txtMarcAgr.Text = "";
+                    return;
+                }
+                marca.Descripcion1 = txtMarcAgr.Text;
+
+                bool existe = negocio.chequear(listaMarcas, marca.Descripcion1);
+                if (!existe)
+                {
+                    negocio.agregarMarca(marca);
+                    MessageBox.Show("Marca agregada con exito");
+                    cargarMarcas();
+                }
+                else
+                {
+                    MessageBox.Show("La marca ya existe");
+                }
+
+                txtMarcAgr.Text = "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            MarcaNegocio negocio = new MarcaNegocio();
+            Marca marcaBorrar;
+
+            try
+            {
+                DialogResult consulta = MessageBox.Show("Eliminas esta marca?", "Eliminando", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+                if (consulta == DialogResult.OK)
+                {
+                    marcaBorrar = (Marca)dgvMarcas.CurrentRow.DataBoundItem;
+                    negocio.eliminarMarca(marcaBorrar);
+                    cargarMarcas();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
+
