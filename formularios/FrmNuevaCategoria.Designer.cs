4fa45668429f0b373c953ffa19bdd5186c16d7cb
patricioSostaric <patricio.jp.01sostaric@gmail.com>
2023-09-14 16:35:10 -0300

    se le agrego funcionalidad al boton agregar categoria

--- formularios/FrmNuevaCategoria.Designer.cs
+++ formularios/FrmNuevaCategoria.Designer.cs
@@ -29,12 +29,113 @@ namespace formularios
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCateg = new System.Windows.Forms.TextBox();
+            this.dgvCategorias = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Categorías:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(328, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Agregar categoría:";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(419, 109);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(71, 25);
+            this.btnBorrar.TabIndex = 15;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(331, 109);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(71, 25);
+            this.btnAgregar.TabIndex = 14;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCateg
+            // 
+            this.txtCateg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCateg.Location = new System.Drawing.Point(331, 71);
+            this.txtCateg.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCateg.MaxLength = 50;
+            this.txtCateg.Name = "txtCateg";
+            this.txtCateg.Size = new System.Drawing.Size(147, 21);
+            this.txtCateg.TabIndex = 13;
+            // 
+            // dgvCategorias
+            // 
+            this.dgvCategorias.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategorias.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvCategorias.Location = new System.Drawing.Point(17, 44);
+            this.dgvCategorias.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvCategorias.MultiSelect = false;
+            this.dgvCategorias.Name = "dgvCategorias";
+            this.dgvCategorias.RowHeadersWidth = 62;
+            this.dgvCategorias.RowTemplate.Height = 28;
+            this.dgvCategorias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCategorias.Size = new System.Drawing.Size(293, 206);
+            this.dgvCategorias.TabIndex = 12;
+            // 
+            // FrmNuevaCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmNuevaCategoria";
+            this.ClientSize = new System.Drawing.Size(551, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCateg);
+            this.Controls.Add(this.dgvCategorias);
+            this.MaximumSize = new System.Drawing.Size(567, 489);
+            this.MinimumSize = new System.Drawing.Size(567, 489);
+            this.Name = "FrmNuevaCategoria";
+            this.Text = "Nueva Categoria";
+            this.Load += new System.EventHandler(this.FrmNuevaCategoria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCateg;
+        private System.Windows.Forms.DataGridView dgvCategorias;
     }
 }
--- formularios/FrmNuevaCategoria.cs
+++ formularios/FrmNuevaCategoria.cs
@@ -7,14 +7,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
 
 namespace formularios
 {
     public partial class FrmNuevaCategoria : Form
     {
+        List<Categoria> listaCategorias = new List<Categoria>();
         public FrmNuevaCategoria()
         {
             InitializeComponent();
         }
+
+        private void FrmNuevaCategoria_Load(object sender, EventArgs e)
+        {
+            cargarCategorias();
+        }
+        private void cargarCategorias()
+        {
+            try
+            {
+                CategoriaNegocio categorias = new CategoriaNegocio();
+                listaCategorias = categorias.listar();
+                dgvCategorias.DataSource = listaCategorias;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Categoria categoria = new Categoria();
+            CategoriaNegocio negocio = new CategoriaNegocio();
+
+            try
+            {
+                if (negocio.validarCampos(txtCateg.Text))
+                {
+                    MessageBox.Show("Debe ingresar una categoría");
+                    return;
+                }
+                if (negocio.validarLetras(txtCateg.Text))
+                {
+                    MessageBox.Show("Ingrese letras, por favor");
+                    txtCateg.Text = "";
+                    return;
+                }
+                categoria.Descripcion1 = txtCateg.Text;
+                bool existe = negocio.chequear(listaCategorias, categoria.Descripcion1);
+                if (!existe)
+                {
+                    negocio.agregarCat(categoria);
+                    MessageBox.Show("Categoría agregada con exito");
+                    cargarCategorias();
+                }
+                else
+                {
+                    MessageBox.Show("La categoría ya existe"); ;
+                }
+                txtCateg.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            CategoriaNegocio negocio = new CategoriaNegocio();
+            Categoria categPorBorrar;
+            try
+            {
+                DialogResult consulta = MessageBox.Show("Eliminas esta categoría?", "Eliminando", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (consulta == DialogResult.OK)
+                {
+                    categPorBorrar = (Categoria)dgvCategorias.CurrentRow.DataBoundItem;
+                    negocio.eliminarCat(categPorBorrar);
+                    cargarCategorias();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
     }
 }
